db1a69a58e423027a0011b7c489122c6af211e0f
Eduardo Silva <eduardo@dudstecnologia.com.br>
2023-11-10 06:12:15 -0400

    Camera in Home admin

--- src/components/HomeAdmin/index.js
+++ src/components/HomeAdmin/index.js
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, Text, TextInput, ActivityIndicator, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import api from '../../services/api';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Camera, useCameraDevice, NoCameraDeviceError } from 'react-native-vision-camera';
 
 export default function HomeAdmin({ navigation }) {
+  const device = useCameraDevice('back');
+
+  const [showCamera, setShowCamera] = useState(false);
+
+  useEffect(() => {
+    async function getPermission() {
+      const newCameraPermission = await Camera.requestCameraPermission();
+
+      if (newCameraPermission == 'granted') {
+        // setShowCamera(true)
+      }
+    }
+    getPermission();
+  }, []);
+
+  if (device == null) {
+    return <NoCameraDeviceError />
+  }
+
   return (
-    <Text style={styles.textBody}>Home Admin</Text>
+    <SafeAreaView style={ styles.container }>
+      <TouchableOpacity style={[styles.buttomDefault, styles.buttonLogin]} onPress={ () => setShowCamera(true) }>
+        <Text style={styles.buttomText}>Capturar QR Code</Text>
+      </TouchableOpacity>
+      <Camera
+        device={device}
+        isActive={showCamera}
+        style={styles.camera}
+      />
+    </SafeAreaView>
   )
 }
--- src/components/HomeAdmin/styles.js
+++ src/components/HomeAdmin/styles.js
@@ -1,8 +1,13 @@
-import { StyleSheet } from 'react-native'
-import general from '../../styles/general'
+import { StyleSheet, Dimensions } from 'react-native';
+const { width }  = Dimensions.get('window');
+import general from '../../styles/general';
 
 const styles = StyleSheet.create({
-  ...general
+  ...general,
+  camera: {
+    width: (width - 20),
+    height: (width - 20)
+  }
 })
 
 export default styles
