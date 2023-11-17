9535c36de7e0eadaff8cfd905ef47ea012950521
Eduardo Silva <eduardo@dudstecnologia.com.br>
2023-11-16 20:21:31 -0400

    Generate QR Code in API

--- src/components/HomeBasic/index.js
+++ src/components/HomeBasic/index.js
@@ -1,15 +1,60 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, Text, TextInput, ActivityIndicator, TouchableOpacity, View } from 'react-native';
-import styles from './styles';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert
+} from 'react-native';
+import styles, { logoFmf } from './styles';
 import api from '../../services/api';
 import Share from 'react-native-share';
 import QRCode from 'react-native-qrcode-svg';
 
 export default function HomeBasic({ navigation }) {
-  let svg = null
+  let qrCode = null;
+  const [qrApi, setQrApi] = useState(null);
+
+  generateQr = (uniqueAccess) => {
+    api.post('/cards/register', { acesso_unico: uniqueAccess })
+    .then(({ data }) => {
+      console.log(data)
+      if (data.qr) {
+        setQrApi(data.qr)
+      } else {
+        alert('Ocorreu um erro ao gerar o Qr Code')
+      }
+    })
+    .catch((error) => {
+      alert('Ocorreu um erro ao gerar o Qr Code')
+      console.log(error.response)
+    })
+  };
+
+  alertGenerate = () => {
+    qrCode = null
+    setQrApi(null)
+
+    Alert.alert('Atenção', 'Qual o tipo de acesso você deseja gerar?', [
+      {
+        text: 'Cancelar',
+        onPress: () => console.log('Nada selecionado')
+      },
+      {
+        text: 'Acesso Único',
+        onPress: () => generateQr(1),
+      },
+      {
+        text: 'Acesso Permanente',
+        onPress: () => generateQr(0),
+        style: 'cancel',
+      }
+    ]);
+  };
 
   saveQRCode = () => {
-    svg.toDataURL(this.callback);
+    qrCode.toDataURL(this.callback);
   };
 
   callback = (dataURL) => {
@@ -17,22 +62,36 @@ export default function HomeBasic({ navigation }) {
       title: 'Auto Security',
       url: `data:image/png;base64,${dataURL}`,
       subject: 'Auto Security',
-    }
+    };
 
     Share.open(shareImageBase64).catch(error => console.log(error));
   }
 
   return (
-    <View>
-      <Text style={styles.textBody}>Home Basic</Text>
-      <TouchableOpacity style={[styles.buttomDefault, styles.buttonLogin]} onPress={ () => saveQRCode() }>
-        <Text style={styles.buttomText}>Enviar QR Code</Text>
-      </TouchableOpacity>
-      <QRCode
-        value="37759871-6e7b-416e-8d6b-72b9c03f5b23"
-        quietZone={5}
-        getRef={(c) => (svg = c)}
-      />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <TouchableOpacity style={styles.buttomDefault} onPress={ alertGenerate }>
+          <Text style={styles.buttomText}>Gerar QR Code</Text>
+        </TouchableOpacity>
+
+        {
+          !qrApi ? <View></View> : <>
+              <View style={styles.containerQr}>
+                <QRCode
+                  value={qrApi}
+                  size={300}
+                  quietZone={5}
+                  logo={{uri: logoFmf}}
+                  getRef={(c) => (qrCode = c)}
+                />
+
+                <TouchableOpacity style={styles.buttomDefault} onPress={ () => saveQRCode() }>
+                  <Text style={styles.buttomText}>Enviar QR Code</Text>
+                </TouchableOpacity>
+              </View>
+            </>
+        }
+      </ScrollView>
+    </SafeAreaView>
   )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,24 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-    baseURL: 'http://172.16.7.121:3000'
+  baseURL: 'http://172.16.7.121:3000'
+});
+
+api.interceptors.request.use(async function (request) {
+  if (!['/login', '/users/register'].includes(request.url)) {
+    try {
+      const token = await AsyncStorage.getItem('token')
+      request.headers.Authorization = `Bearer ${token}`;
+    } catch (e) {
+      console.log('Erro ao ler o token')
+      console.log(e)
+    }
+  }
+
+  return request;
+}, function (error) {
+  return Promise.reject(error);
 });
 
 export default api;
